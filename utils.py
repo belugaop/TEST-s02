ec47403e0df0f566d53d931b7dc5a614f90f5816
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-07-20 10:15:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,14 +382,14 @@ def humanbytes(size):
 ####################  droplink  ####################
 async def droplink_url(link, x=""):
     if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
-        text = f'https://droplink.co/st?api={DROPLINK_API}&url={url}'
+        text = f'https://straightlink.online/st?api={DROPLINK_API}&url={url}'
         return text
     else:
         https = link.split(":")[0]
         if "http" == https:
             https = "https"
             link = link.replace("http", https)
-        url = f'https://droplink.co/api'
+        url = f'https://straightlink.online/api'
         params = {'api': DROPLINK_API,
                 'url': link,
                 'alias': x
@@ -406,7 +406,7 @@ async def droplink_url(link, x=""):
 
         except Exception as e:
             logger.error(e)
-            links = f'https://droplink.co/st?api={DROPLINK_API}&url={link}'
+            links = f'https://straightlink.online/st?api={DROPLINK_API}&url={link}'
             return await tiny_url_main(links)
 
 
